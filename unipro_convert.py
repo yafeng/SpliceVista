a9499f04f51f0861ab82df602940733a928f25f7
Yafeng Zhu <yafengzhu@asambp.scilifelab.se>
2013-02-22 17:21:17 +0100

    fix bug

--- unipro_convert.py
+++ unipro_convert.py
@@ -1,11 +1,12 @@
 import sys
 
-def getprotein(infile): #get protein id list out of ms file 
-    lis={}
-    for line in infile:
-        protein_acc=line.split('\t')[0]
-        lis[protein_acc]=1
-    return lis;
+def normalize(lis):
+    newlis=[]
+    for i in range(0,len(lis)):
+        normvalue=float(lis[i])/float(lis[0])
+        newlis.append(normvalue)
+    
+    return newlis
 
 handle=open("Uniproid_Gene.txt",'r')
 uniproid_gene={}
@@ -15,9 +16,18 @@ for line in handle:
 
 msoutput=sys.argv[1]
 input_file=open(msoutput,'r')
-acclist=getprotein(input_file)
+acclist={}
+totalPSM=0
+for line in input_file:
+    totalPSM+=1
+    protein_acc=line.split('\t')[0]
+    if protein_acc not in acclist:
+        acclist[protein_acc]=1
+
+print totalPSM,'PSMs in this  file'
+print len(acclist),'proteins'
 input_file.close()
-print len(acclist)
+
 N=0
 for protein_acc in acclist.keys():
     proteinList=protein_acc.split(";")
@@ -25,7 +35,11 @@ for protein_acc in acclist.keys():
     for i in range(0,len(proteinList)):
         proteinID=proteinList[i]
         try:
-            genename.append(uniproid_gene[proteinID])
+            if '-' not in proteinID:
+                genename.append(uniproid_gene[proteinID])
+            else:
+                index=proteinID.index('-')
+                genename.append(uniproid_gene[proteinID[:index]])
         
         except KeyError:
             genename.append('None')
@@ -56,9 +70,13 @@ outfile.write("\t".join(cols))
 
 for line in infile2:
     cols=line.split('\t')
+    intensity=cols[2:]
+    ratio=normalize(intensity)
+    ratio_round=[ '%.2f' % elem for elem in ratio ] #round the value, keep 2 digit after decimal
     proteinID=cols[0]
     cols.insert(1,acclist[proteinID])
-    outfile.write("\t".join(cols))
+    newcols=cols[:3]+ratio_round
+    outfile.write("\t".join(newcols)+'\n')
 
 print 'file including gene symbol saved'    
 outfile.close()
